df6b4361eaf71d158f37f59cd89d37feb874e95e
askl56 <amscott@openmailbox.org>
2015-05-20 03:49:45 +0100

    added devise and fixed factories. All tests passing thus far

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,8 @@ class Account < ActiveRecord::Base
     format: { with: /\A[a-z][a-z0-9\-]+[a-z0-9]\Z/ },
     uniqueness: true
 
+  has_many :memberships, dependent: :destroy
+  has_many :users, through: :memberships
+  has_one :owner_membership, -> { where(role: 'owner') }, class_name: 'Membership'
+  has_one :owner, through: :owner_membership, source: :user, class_name: 'User'
 end
