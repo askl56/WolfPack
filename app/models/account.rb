a316a90fecd2c93f32a9dc20885970557d5bd814
askl56 <amscott@openmailbox.org>
2015-05-20 03:57:04 +0100

    added advanced controller functions and tests for them

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,4 +8,5 @@ class Account < ActiveRecord::Base
   has_many :users, through: :memberships
   has_one :owner_membership, -> { where(role: 'owner') }, class_name: 'Membership'
   has_one :owner, through: :owner_membership, source: :user, class_name: 'User'
+  accepts_nested_attributes_for :owner
 end
